061b9f13f64af776b82775fd35d5a481a04d1213
amr <amr514863@gmail.com>
2024-10-27 20:08:21 +0300

    booking validation added

--- src/modules/bookings/booking.routes.ts
+++ src/modules/bookings/booking.routes.ts
@@ -9,22 +9,30 @@ import { allowedTo } from "../../middlewares/auth";
 import { Roles } from "../user/Roles";
 import { createCheckOutSessions } from "../../utils/paymets";
 import { verfifyToken } from "../../middlewares/verifiyToken";
+import {
+  bookVenueValidator,
+  cancelBookingValidator,
+} from "./booking.validator";
+import { validateRequest } from "../../middlewares/validate-request";
 
 export const bookingRouter = Router();
 bookingRouter
-.get("/success-payment/:id", displayMessage)
+  .get("/success-payment/:id", displayMessage)
 
-.use(verfifyToken)
-.post(
-  "/book-venue/:id",
-  allowedTo(Roles.USER),
-  bookVenue,
-  createCheckOutSessions
-)
-.patch(
-  "/cancel-booking/:id",
-  allowedTo(Roles.USER),
-  cancelBooking
-)
-.get("/get-user-bookings", allowedTo(Roles.USER), getUserBookings);
-
+  .use(verfifyToken)
+  .post(
+    "/book-venue/:id",
+    allowedTo(Roles.USER),
+    bookVenueValidator,
+    validateRequest,
+    bookVenue,
+    createCheckOutSessions
+  )
+  .patch(
+    "/cancel-booking/:id",
+    allowedTo(Roles.USER),
+    cancelBookingValidator,
+    validateRequest,
+    cancelBooking
+  )
+  .get("/get-user-bookings", allowedTo(Roles.USER), getUserBookings);
